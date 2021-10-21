a8f129894a0b18ab63136aa6379e777168ab3798
Liam Wang <skid-scales.0a@icloud.com>
2021-10-21 11:10:07 +0800

    更新

--- install_packages.sh
+++ install_packages.sh
@@ -5,20 +5,24 @@ opkg install luci-i18n-base-zh-cn
 opkg install luci-i18n-opkg-zh-cn
 
 # 安装 ddns
-opkg install luci-app-ddns
-opkg install luci-i18n-ddns-zh-cn
+opkg install luci-app-ddns luci-i18n-ddns-zh-cn
 opkg install ddns-scripts-cloudflare
 
 # Linksys
 opkg install luci-app-advanced-reboot
 
 # 网络唤醒
-opkg install luci-app-wol
-opkg install luci-i18n-wol-zh-cn
+opkg install luci-app-wol luci-i18n-wol-zh-cn
 
 # banIP
-opkg install luci-banip
-opkg install luci-i18n-banip-zh-cn
+opkg install luci-banip luci-i18n-banip-zh-cn
 
 # WireGuard
-opkg install luci-proto-wireguard luci-app-wireguard luci-i18n-wireguard-zh-cn wireguard-tools qrencode
+opkg install luci-proto-wireguard wireguard-tools qrencode
+opkg install luci-app-wireguard luci-i18n-wireguard-zh-cn
+
+# ACME
+opkg install luci-app-acme luci-i18n-acme-zh-cn
+
+# 安装常用包
+opkg install git-http
--- set_opkg_mirrors.sh
+++ set_opkg_mirrors.sh
@@ -1,14 +1,17 @@
 #!/bin/ash
 
+# 安装 wget-ssl 让更换源能正常下载包数据
+opkg update
+opkg install wget-ssl
+
 # 备份
 distfeeds_file=/etc/opkg/distfeeds.conf
-if [ ! -f "${distfeeds_file}.backup" ]; then
-    cp $distfeeds_file "${distfeeds_file}.backup"
+if [ ! -f "${distfeeds_file}.origin" ]; then
+    cp $distfeeds_file "${distfeeds_file}.origin"
 fi
 
 # 更换软件源
 # https://developer.aliyun.com/mirror/openwrt
 # 编辑 /etc/opkg/distfeeds.conf 文件，将其中的 downloads.openwrt.org 替换为 mirrors.aliyun.com/openwrt 即可
 sed -i 's_downloads.openwrt.org_mirrors.aliyun.com/openwrt_' $distfeeds_file
-
 #sed -i 's_downloads.openwrt.org_mirrors.tuna.tsinghua.edu.cn/openwrt_' $distfeeds_file
