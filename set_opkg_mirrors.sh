e7ba5e6ade0b8f4fb41823aac5f7210040a67228
Liam Wang <liamwang@aliyun.com>
2022-05-08 21:47:52 +0800

    update set mirror script

--- set_opkg_mirrors.sh
+++ set_opkg_mirrors.sh
@@ -4,14 +4,16 @@
 opkg update
 opkg install wget-ssl
 
-# 备份
-distfeeds_file=/etc/opkg/distfeeds.conf
-if [ ! -f "${distfeeds_file}.origin" ]; then
-    cp $distfeeds_file "${distfeeds_file}.origin"
-fi
-
 # 更换软件源
 # https://developer.aliyun.com/mirror/openwrt
 # 编辑 /etc/opkg/distfeeds.conf 文件，将其中的 downloads.openwrt.org 替换为 mirrors.aliyun.com/openwrt 即可
-sed -i 's_downloads.openwrt.org_mirrors.aliyun.com/openwrt_' $distfeeds_file
-#sed -i 's_downloads.openwrt.org_mirrors.tuna.tsinghua.edu.cn/openwrt_' $distfeeds_file
+source_file=/etc/opkg/distfeeds.conf
+default_host=downloads.openwrt.org
+mirror_host=mirrors.aliyun.com/openwrt
+# mirror_host=mirrors.tuna.tsinghua.edu.cn/openwrt
+# mirror_host=mirror.sjtu.edu.cn/openwrt
+
+if [ ! -f "${source_file}.backup" ]; then
+  cp $source_file "${source_file}.backup"
+  sed -i "s_${default_host}_${mirror_host}_" $source_file
+fi
