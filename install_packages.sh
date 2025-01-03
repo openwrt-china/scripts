c7e580e97fe65918d2dc35aa8147ce4961dfcedb
Liam Wang <liamwang@aliyun.com>
2025-01-03 14:07:17 +0800

    更新脚本

--- install_packages.sh
+++ install_packages.sh
@@ -5,14 +5,16 @@ opkg install luci-i18n-base-zh-cn
 opkg install luci-i18n-opkg-zh-cn
 
 # 安装 ddns
-opkg install luci-app-ddns luci-i18n-ddns-zh-cn
-opkg install ddns-scripts-cloudflare
+opkg install luci-app-ddns luci-i18n-ddns-zh-cn ddns-scripts-cloudflare
 
 # 网络唤醒
 opkg install luci-app-wol luci-i18n-wol-zh-cn
 
 # banIP
 opkg install luci-banip luci-i18n-banip-zh-cn
+
+# WireGuard
+opkg install wireguard-tools kmod-wireguard luci-app-wireguard
 
 # ACME
 opkg install luci-app-acme luci-i18n-acme-zh-cn
